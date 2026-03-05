d3f22cc7c8d5c62ce6210fb9f1a415b44667d33c
Fernanda Passos <fernanda.passos@isel.pt>
2026-03-05 18:28:57 +0000

    Exemplos de conversões numéricas de primitivos (Java e Kotlin)

---
+++ week03/primitive-conversions-samples/java/Test.java
@@ -0,0 +1,7 @@
+class Test {
+	public static void main(String[] args){
+		int i = 7;
+		long k = i; // widening conversion
+		int j = (int) k; // narrowing conversion
+	}
+}
---
+++ week03/primitive-conversions-samples/kotlin/Test.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val i: Int = 7
+    val l: Long = i.toLong()
+    val j: Int = l.toInt()
+}

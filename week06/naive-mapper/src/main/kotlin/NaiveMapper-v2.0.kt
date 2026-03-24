27d2e12e9fb0482b016166828a38391303a449e5
fgopassos <fernanda.passos@isel.pt>
2026-03-24 11:30:54 +0000

    NaiveMapper with same property names as extension function.

--- week06/naive-mapper/src/main/kotlin/NaiveMapper-v2.0.kt
+++ week06/naive-mapper/src/main/kotlin/NaiveMapper-v2.0.kt
@@ -17,33 +17,32 @@ fun <T : Any> Any.mapTo(dest: KClass<T>) : T {
      * 1st - selects the constructor with all mandatory parameters
      * in the properties of the source.
      */
-    val destCtor = dest
-        .constructors
+    val destCtor = dest.constructors
         .first { ctor ->
-            ctor
-                .parameters
-                .filter { !it.isOptional }
-                .all { param -> this::class
-                    .memberProperties
-                    .any { it.name == param.name && it.returnType == param.type}
+            ctor.parameters
+                .filter { !it.isOptional } // only mandatory parameters
+                .all { param -> // All parameters with same properties in the source
+                    val srcProps = this::class.memberProperties
+                    // Any property with same name and type
+                    srcProps.any { it.name == param.name && it.returnType == param.type }
                 }
         }
 
-    destCtor.parameters.forEach { println("${it.name}: ${it.type}") }
+    // destCtor.parameters.forEach { println("${it.name}: ${it.type}") } // logger
     /**
      * 2nd - look for matching properties with constructor parameters
+     * and get the source object value.
      * callBy needs a Map<KParameter, Any?> to be called.
      */
-    val args: Map<KParameter, Any?> = destCtor
-        .parameters
-        .associateWith { param -> this::class
-            .memberProperties
-            .firstOrNull() { it.name == param.name && it.returnType == param.type }
-            ?.call(this)
+    val args: Map<KParameter, Any?> = destCtor.parameters
+        .associateWith { param ->
+            this::class.memberProperties
+                .firstOrNull { it.name == param.name && it.returnType == param.type }
+                ?.call(this)
         }
         .filter { it.value != null }
 
-    args.forEach { (k, v) -> println("${k.name} -> $v") }
+    // args.forEach { (k, v) -> println("${k.name} -> $v") } // logger
     /**
      * 3rd create the instance of dest via constructor using
      * the callBy<Map<KParameter, Any>>
